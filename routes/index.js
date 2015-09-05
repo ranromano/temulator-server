ec939343c12fa705e0ebdb7bd748a9a4c6eb9423
Ran Romano <ranromano@outlook.com>
2015-09-05 15:46:55 +0300

    Adding half-baked user authentication

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,42 @@
 var express = require('express');
+var isAuthenticated = require('../user_repo/passportUtils').isAuthenticated;
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+module.exports = function(passport) {
 
-module.exports = router;
+    // TODO: for testing, remove when going live
+    router.get('/', function(req, res, next) {
+        res.send('respond with a resource');
+    });
+
+    /**
+     *  User Registration.
+     */
+    router.post('/signup', passport.authenticate('signup', {
+        // TODO: refactor using redirects
+        successRedirect: '/login',
+        failureRedirect: '/signup',
+        failureFlash : true
+    }));
+
+    /**
+     *  User login.
+     */
+    router.post('/login', passport.authenticate('login', {
+        // TODO: refactor using redirects
+        successRedirect: '/home',
+        failureRedirect: '/',
+        failureFlash : true
+    }));
+
+    /**
+     * Rank player
+     */
+    // TODO: Rank player Endpoint
+
+    // TODO: Add player Endpoint
+
+    // TODO: Push notifications Endpoints
+
+    return router;
+};
---
+++ user_repo/db.js
@@ -0,0 +1,3 @@
+module.exports = {
+    'url'   : 'mongodb://teamulator-server:zaq1XSW2@ds055822.mongolab.com:55822/teamulator'
+};
