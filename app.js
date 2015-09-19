384378ce800f5848f1a3a9f5d9939b904852b9f7
Ran Romano <ranromano@outlook.com>
2015-09-19 21:49:47 +0300

    Allowing cross domains

--- app.js
+++ app.js
@@ -12,6 +12,15 @@ var expressSession = require('express-session');
 
 var app = express();
 
+var allowCrossDomain = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+  next();
+};
+
+app.use(allowCrossDomain);
 app.use(expressSession({secret: 'rromano'}));
 app.use(passport.initialize());
 app.use(passport.session());
